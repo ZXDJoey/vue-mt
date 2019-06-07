06890480ac2811749049ce14ae0945ddef94fa87
zxdjoey <774793337@qq.com>
2019-06-07 22:38:35 +0800

    header developing

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,9 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/reset.css',
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/main.css'
   ],
 
   /*
