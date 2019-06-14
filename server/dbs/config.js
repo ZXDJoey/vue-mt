37739175256cf4ec153068224250bb4422a44222
zhangxiaodong <774793337@qq.com>
2019-06-14 15:49:37 +0800

    Update config.js

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -16,17 +16,17 @@ export default {
       return '774793337@qq.com'
     },
     get pass () {
-      return 'xxxxxxxxxxxx'
-    }
-  },
-  get code () {
-    return () => {
-      return Math.random().toString(16).slice(2, 6).toUpperCase()
-    }
-  },
-  get expire () {
-    return () => {
-      return new Date().getTime() + 60 * 60 * 1000
+      return 'xiaodong@20'
+    },
+    get code () {
+      return () => {
+        return Math.random().toString(16).slice(2, 6).toUpperCase()
+      }
+    },
+    get expire () {
+      return () => {
+        return new Date().getTime() + 60 * 60 * 1000
+      }
     }
   }
 }
