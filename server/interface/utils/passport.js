6c14aedf76b44768f42872fcbaacab3728c22749
zhangxiaodong <774793337@qq.com>
2019-06-14 14:10:08 +0800

    Update passport.js

--- server/interface/utils/passport.js
+++ server/interface/utils/passport.js
@@ -2,12 +2,12 @@ import passport from 'koa-passport'
 import LocalStrategy from 'passport-local'
 import UserModel from '../../dbs/models/users'
 
-passport.use(new LocalStrategy(function (username, password, done) {
+passport.use(new LocalStrategy(async function (username, password, done) {
   let where = {
     username
   }
   let result = await UserModel.findOne(where)
-  if (result !== null) {
+  if (result != null) {
     if (result.password === password) {
       return done(null, result)
     } else {
