0b0e1cfe5d596809fe4044dde59d49da861f4870
zhangxiaodong <774793337@qq.com>
2019-06-17 11:31:57 +0800

    Update geo.vue

--- components/public/header/geo.vue
+++ components/public/header/geo.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="m-geo">
-    <i class="el-icon-location">北京市</i>
+    <i class="el-icon-location">{{ city }}</i>
     <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
-
+  ...mapState({
+    city: state => state.geo.position
+  })
 }
 </script>
 
