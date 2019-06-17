da084e8d2050f933579d3335ba6db3e52a803634
zhangxiaodong <774793337@qq.com>
2019-06-17 11:32:32 +0800

    Create index.js

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import geo from './modules/geo'
+
+Vue.use(Vuex)
+
+const store = () => new Vuex.Store({
+  modules: {
+    geo
+  },
+  actions: {
+    async nuxtServerInit({ commit }, { req, app }) {
+      const { status, data: { province, city } } = await app.$axios.get('/geo/getPosition')
+    }
+  }
+})
+
+export default store
