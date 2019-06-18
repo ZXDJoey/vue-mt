d416f3423967546dee642ddf4d90d714d0488f0e
zhangxiaodong <774793337@qq.com>
2019-06-18 17:24:38 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,60 +1,73 @@
-import Koa from "koa";
-import consola from "consola";
-const { Nuxt, Builder } = require('nuxt')
+import Koa from 'koa'
+import mongoose from 'mongoose'
+import consola from 'consola' // Elegant Console Logger for Node.js and Browser
+import bodyParser from 'koa-bodyparser' // Parsing the middleware of the body, this.body can get the data directly in koa. Post parameter acquisition
+import session from 'koa-generic-session' // supports Delay session getter
+import Redis from 'koa-redis' // Redis storage for koa session middleware/cache.
+import json from 'koa-json' // JSON pretty-printed response middleware.
 
-import mongoose from "mongoose"
-import bodyParser from "koa-bodyparser"
-import session from "koa-generic-session"
-import Redis from "koa-redis"
-import json from "koa-json"
-import dbConfig from "./dbs/config"
-import passport from "./interface/utils/passport"
-import users from "./interface/users"
+import dbConfig from './dbs/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
 import geo from './interface/geo'
 import search from './interface/search'
+import category from './interface/category'
+import cart from './interface/cart'
+import order from './interface/order'
 
+const { Nuxt, Builder } = require('nuxt')
 const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
-app.keys = ["mt", "keyskeys"]
+// session
+app.keys = ['mt', 'keyskeys']
 app.proxy = true
-app.use(session({
-  key: "mt",
-  prefix: "mt:uid",
-  store: new Redis()
-}))
-
-app.use(bodyParser({
-  extendTypes: ["json", "form", "text"]
-}))
-app.use(json())
-
-mongoose.connect(dbConfig.dbs, {
-  useNewUrlParser: true
-})
+app.use(
+  session({
+    key: 'mt',
+    prefix: 'mt:uid',
+    store: new Redis()
+  })
+)
+// open koa-passport Support for session
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use( // post handle
+  bodyParser({
+    extendTypes: ['json', 'form', 'text']
+  })
+)
+
+app.use(json()) // data pretty
+
+mongoose.connect( // Connect to the database
+  dbConfig.dbs,
+  {
+    useNewUrlParser: true
+  }
+)
+
 // Import and Set Nuxt.js options
-let config = require('../nuxt.config.js')
+const config = require('../nuxt.config.js')
 config.dev = !(app.env === 'production')
 
 async function start() {
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
-
-  // Build in development
-  if (config.dev) {
+  const nuxt = new Nuxt(config) // Instantiate nuxt.js
+  if (config.dev) { // Build in development
     const builder = new Builder(nuxt)
     await builder.build()
   }
+  // Introducing routing
   app.use(users.routes()).use(users.allowedMethods())
   app.use(geo.routes()).use(geo.allowedMethods())
   app.use(search.routes()).use(search.allowedMethods())
+  app.use(category.routes()).use(category.allowedMethods())
+  app.use(cart.routes()).use(cart.allowedMethods())
+  app.use(order.routes()).use(order.allowedMethods())
   app.use(ctx => {
     ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-
     return new Promise((resolve, reject) => {
       ctx.res.on('close', resolve)
       ctx.res.on('finish', resolve)
@@ -64,7 +77,6 @@ async function start() {
       })
     })
   })
-
   app.listen(port, host)
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
