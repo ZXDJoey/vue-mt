463e09152c304727e1dc7710662e1ca78c69738f
zhangxiaodong <774793337@qq.com>
2019-06-19 15:09:06 +0800

    Update searchbar.vue

--- components/public/header/searchbar.vue
+++ components/public/header/searchbar.vue
@@ -1,52 +1,113 @@
 <template>
   <div class="search-panel">
     <el-row class="m-header-searchbar">
-      <el-col :span="3" class="left">
-        <img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png" alt="美团">
+      <el-col
+        :span="3"
+        class="left"
+      >
+        <nuxt-link to="/">
+          <img
+            src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png"
+            alt="美团"
+          >
+        </nuxt-link>
       </el-col>
-      <el-col :span="15" class="center">
+      <el-col
+        :span="15"
+        class="center"
+      >
         <div class="wrapper">
-          <el-input v-model="search" placeholder="搜索商家或地点" @focus="focus" @blur="blur" @input="input"></el-input>
+          <el-input
+            v-model="search"
+            placeholder="搜索商家或地点"
+            @focus="focus"
+            @blur="blur"
+            @input="input"
+          />
           <button class="el-button el-button--primary">
-            <i class="el-icon-search"></i>
+            <i class="el-icon-search"/>
           </button>
-          <dl class="hotPlace" v-if="isHotPlace">
+          <dl
+            v-if="isHotPlace"
+            class="hotPlace"
+          >
             <dt>热门搜索</dt>
-            <dd v-for="(item, idx) in hotPlace" :key="idx">{{ item }}</dd>
+            <dd
+              v-for="(item, index) in $store.state.search.hotPlace.slice(0, 4)"
+              :key="index"
+            >
+              <a :href="'/products?keyword=' + encodeURIComponent(item.name)">{{ item.name }}</a>
+            </dd>
           </dl>
-          <dl class="searchList" v-if="isSearchList">
-            <dd v-for="(item, idx) in searchList" :key="idx">{{ item }}</dd>
+          <dl
+            v-if="isSearchList"
+            class="searchList"
+          >
+            <dd
+              v-for="(item, index) in searchList"
+              :key="index"
+            >
+              <a :href="'/products?keyword=' + encodeURIComponent(item.name)">{{ item.name }}</a>
+            </dd>
           </dl>
         </div>
-        <p class="suggset">
-          <a href="">1</a>
-        </p>
+        <div class="suggset">
+          <a
+            v-for="(item, index) in $store.state.search.hotPlace.slice(0, 4)"
+            :key="index"
+            :href="'/products?keyword=' + encodeURIComponent(item.name)"
+          >{{ item.name }}</a>
+        </div>
         <ul class="nav">
           <li>
-            <nuxt-link to="/" class="takeout">美团外卖</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="takeout"
+            >美团外卖</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="movie">猫眼电影</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="movie"
+            >猫眼电影</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="hotel">美团酒店</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="hotel"
+            >美团酒店</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="apartment">民宿/公寓</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="apartment"
+            >民宿/公寓</nuxt-link>
           </li>
           <li>
-            <nuxt-link to="/" class="business">商家入驻</nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/" class="business">美团公益</nuxt-link>
+            <nuxt-link
+              to="/"
+              class="business"
+            >商家入驻</nuxt-link>
           </li>
         </ul>
       </el-col>
-      <el-col :span="6" class="right">
+      <el-col
+        :span="6"
+        class="right"
+      >
         <ul class="security">
-          <li><i class="refund"><p class="txt">随时退</p></i></li>
-          <li><i class="single"><p class="txt">不满意免单</p></i></li>
-          <li><i class="overdue"><p class="txt">过时退</p></i></li>
+          <li>
+            <i class="refund"/>
+            <p class="txt">随时退</p>
+          </li>
+          <li>
+            <i class="single"/>
+            <p class="txt">不满意免单</p>
+          </li>
+          <li>
+            <i class="overdue"/>
+            <p class="txt">过期退</p>
+          </li>
         </ul>
       </el-col>
     </el-row>
@@ -54,41 +115,54 @@
 </template>
 
 <script>
+import _ from 'lodash'
 export default {
-  data () {
+  data() {
     return {
       search: '',
       isFocus: false,
       hotPlace: [],
-      searchList: []
+      searchList: [],
+      inputValue: false
     }
   },
   computed: {
-    isHotPlace () {
+    isHotPlace() {
       return this.isFocus && !this.search
     },
-    isSearchList () {
-      return this.isFocus && this.search
+    isSearchList() {
+      return this.isFocus && this.inputValue
     }
   },
   methods: {
-    focus () {
+    focus() {
       this.isFocus = true
     },
-    blur () {
-      let self = this
+    blur() {
       setTimeout(() => {
         this.isFocus = false
-      }, 300)
+        this.inputValue = false
+      }, 200)
     },
-    input () {
-      
-    }
+    input: _.debounce(async function() { // _.debounce: Delay function
+      this.searchList = []
+      const city = this.$store.state.geo.position.city.replace('市', '')
+      const { data: { top }} = await this.$axios.get('/search/top', {
+        params: { // pass to search.js parameter
+          input: this.search,
+          city
+        }
+      })
+      this.searchList = top.slice(0, 10)
+      if (this.search === '') {
+        this.inputValue = false
+      } else {
+        this.inputValue = true
+      }
+    }, 300)
   }
 }
 </script>
 
-<style lang="scss">
-
+<style scoped lang="scss">
 </style>
-
